82331c981af19b0a69849bada87e1625c0ce96ed
Umairadox03360353802 <umairadox033@gmail.com>
2021-03-21 01:41:39 +0500

    www

---
+++ ajax.js
@@ -0,0 +1,28 @@
+console.log("my name is adox and you");
+
+
+// let fetchBtn = document.getElementById('fetchBtn');
+// fetchBtn.addEventListener('click', buttonClickHandler)
+
+// function buttonClickHandler() {
+//     console.log('You have clicked the fetchBtn');
+
+//     // Instantiate an xhr object
+//     const xhr = new XMLHttpRequest();
+
+//     // Open the object
+//     xhr.open('GET', '/./thomas.text', true);
+
+//     xhr.onprogress = function () {
+//         console.log('On progress');
+//     }
+//     // What to do when response is ready
+//     xhr.onload = function () {
+
+//         console.log(this.responseText)
+//     }
+
+//     // send the request
+//     xhr.send();
+//     console.log('hello this has been started');
+// }
